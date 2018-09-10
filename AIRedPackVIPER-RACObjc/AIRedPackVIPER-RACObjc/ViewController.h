f45761ca9771fafbe53d1d5bf04337fb086ae4ee
艾泽鑫 <443792431@qq.com>
2018-09-10 21:36:05 +0800

    init

---
+++ AIRedPackVIPER-RACObjc/AIRedPackVIPER-RACObjc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AIRedPackVIPER-RACObjc
+//
+//  Created by 艾泽鑫 on 2018/9/10.
+//  Copyright © 2018年 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AIRedPackVIPER-RACObjc/AIRedPackVIPER-RACObjcTests/AIRedPackVIPER_RACObjcTests.m
@@ -0,0 +1,39 @@
+//
+//  AIRedPackVIPER_RACObjcTests.m
+//  AIRedPackVIPER-RACObjcTests
+//
+//  Created by 艾泽鑫 on 2018/9/10.
+//  Copyright © 2018年 艾泽鑫. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AIRedPackVIPER_RACObjcTests : XCTestCase
+
+@end
+
+@implementation AIRedPackVIPER_RACObjcTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
